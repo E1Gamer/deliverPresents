f5c5655f8f4900e4bd5707cec4dde2f612d46eda
Chayra Perez <prgirl@live.com>
2019-09-23 20:12:22 -0400

    Santa Came to Town

--- santa.js
+++ santa.js
@@ -1,5 +1,10 @@
 function deliverPresents(houses) {
+  // Base case
   console.log('Delivering presents to ' + houses[0])
+
+  // Break the problem down into smaller piece(s)
+
+  // Recall the function on the smaller piece(s)
 }
 
 module.exports = deliverPresents
